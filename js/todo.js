5d776470e55440d87d0addf5b5a18d711180007c
JiYoon Lee <leeyoon97@naver.com>
2025-04-06 23:47:46 +0900

    list create

--- js/todo.js
+++ js/todo.js
@@ -1,28 +1,46 @@
-const todo_input = document.querySelector("#todo_input");
-const todoCheck = document.querySelector("#todo_checkbox");
-const addBtn = document.querySelector(".add_button");
-const completeBtn = document.querySelector(".complete_button");
-const renameBtn = document.querySelector(".rename");
-const deleteBtn = document.querySelector(".delete_button");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.querySelector("#todo_input");
 
-let todolist = [];
+  const todoCheck = document.querySelector("#todo_checkbox");
 
-function setting() {
-  storageLoad(); // 로컬 저장소 불러오기
-  addBtn.addEventListener("click", createList);
-}
+  const addBtn = document.querySelector(".add_button");
 
-setting();
+  const completeBtn = document.querySelector(".complete_button");
 
-function storageLoad() {
-  const savedTodo = localStorage.getItem("TODO");
-  if (savedTodo != null) {
-    const TodoList = JSON.parse(savedTodo);
-    TodoList.forEach((todo) => {
-      const { text } = todo;
-      const { checked } = todo;
-      printTodo(text, checked);
-      savedTodo(text, checked);
-    });
+  const renameBtn = document.querySelector(".rename");
+
+  const deleteBtn = document.querySelector(".delete_button");
+
+  let todolist = [];
+
+  function setting() {
+    storageLoad(); // 로컬 저장소 불러오기
+    addBtn.addEventListener("click", createList);
   }
-}
+
+  setting();
+
+  function storageLoad() {
+    const savedTodo = localStorage.getItem("TODO");
+    if (savedTodo != null) {
+      const TodoList = JSON.parse(savedTodo);
+      TodoList.forEach((todo) => {
+        const { text } = todo;
+        const { checked } = todo;
+        printTodo(text, checked);
+        saveTodo(text, checked);
+      });
+    }
+  }
+  function creatList(e) {
+    e.preventDefault();
+    const todoValue = todoInput.value;
+    if (todoValue == "") {
+      return;
+    } else {
+      printTodo(todoValue, 0);
+      saveTodo(todoValue, 0);
+      todoInput.value = "";
+    }
+  }
+});

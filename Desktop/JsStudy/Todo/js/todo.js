219a654c53df43559d836d7652821a47bcb60aaa
JiYoon Lee <leeyoon97@naver.com>
2025-04-06 22:31:39 +0900

    new readme file

---
+++ Desktop/JsStudy/Todo/js/todo.js
@@ -0,0 +1,9 @@
+const todo_input = document.querySelector("#todo_input");
+const todoCheck = document.querySelector("#todo_checkbox");
+const addBtn = document.querySelector(".add_button");
+const completeBtn = document.querySelector(".complete_button");
+const renameBtn = document.querySelector(".rename");
+const deleteBtn = document.querySelector(".delete_button");
+
+let todolist = []
+
